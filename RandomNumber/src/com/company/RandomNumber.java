87808c4b43b873dec019052ad67eded4bb9cbe0a
Lori <lori.imbesi@gmail.com>
2019-04-02 14:09:38 -0400

    Add files via upload

---
+++ RandomNumber/src/com/company/RandomNumber.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Random;
+
+public class RandomNumber {
+
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <= 0; i++)
+
+        {
+            //Generate a random number from 0 to 9
+
+            int randomNum = (int) (Math.random() * 10);
+            System.out.println("Here's a random number: "
+                    + randomNum);
+
+        }
+
+        System.out.println();
+    }
+}
